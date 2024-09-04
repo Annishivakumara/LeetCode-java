1e3d8a8ae619608d31e09cb9e74524b513014aff
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-04 22:13:16 +0530

    Create PrintNumber(n(1 to N)times.java
    Input:
    5
    Output:
    GFG GFG GFG GFG GFG

---
+++ gfg/PrintNumber(n(1 to N)times.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    void printGfg(int N) {
+        if(N==0){
+            return;
+        }// code here
+        System.out.print("GFG ");
+      
+        printGfg(N-1);
+    }
+}
