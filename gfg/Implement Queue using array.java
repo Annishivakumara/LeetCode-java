2872096998c087b0547f3ca43a05a68971d9a0c0
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-26 00:04:04 +0530

    Update Implement Queue using array.java

--- gfg/Implement Queue using array.java
+++ gfg/Implement Queue using array.java
@@ -12,32 +12,23 @@ class MyQueue {
 	//Function to push an element x in a queue.
 	void push(int x)
 	{
-	   if(rear==arr.length-1){
-	    System.out.println("Queue is Full");
-	   }
-	   else if(front==-1 && rear==-1){
-	       front=rear=0;
-	   }
-	   else{
-	       rear++;
-	       arr[rear]=x;
-	   }
+        rear++;
+        arr[rear]=x;
 	} 
 
     //Function to pop an element from queue and return that element.
 	int pop()
 	{
-	if(front==-1 && rear==-1){
-	    return -1;
-	}
-	else if(rear==front){
-	     front = rear=-1;
-	     return -1;
-	}
-	else{
-	    front++;
-	    return arr[front];
-	}
-
+	  if(rear==-1 && front==-1){
+	      return -1;
+	  }
+	  else if(rear==front){
+	    rear=front=-1;
+	    return rear;
+	    
+	  }
+	  else{
+	    return arr[++front];
+	  }
 	} 
 }
